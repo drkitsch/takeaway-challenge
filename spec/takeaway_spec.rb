851bcca38fb1fb34c51837fd224e506859cff427
Harry Parsons <hparsons93@gmail.com>
2021-09-04 13:33:00 +0100

    13:32

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,7 +4,7 @@ describe Takeaway do
   subject(:takeaway) { described_class.new(menu: menu, order: order) }
 
   let(:menu) { double(:menu, print: printed_menu) }
-  let(:order) { double(:order) }
+  let(:order) { double(:order, total: 15.50) }
   let(:printed_menu) { "Chicken: £3.50"}
 
   let(:dishes) { {chicken: 2, fish: 1} }
@@ -16,6 +16,11 @@ describe Takeaway do
   it "can order some number of several available dishes" do 
   expect(order).to receive(:add).twice
   takeaway.place_order(dishes)
+  end 
+
+  it "knows the order total" do 
+    total = takeaway.place_order(dishes)
+    expect(total).to eq(15.50)
   end 
 
 
